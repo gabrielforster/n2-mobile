7acff1cd58929723ebbf84e584428952c0462ad1
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-10-28 23:11:42 -0300

    feat: changing colors

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# n2_mobile_tassiana
+# N2 Desenvolvimento Mobile
 
-A new Flutter project.
+### Paleta de cor
+https://coolors.co/363635-595a4a-b0fe76-81e979-8fbb99
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 import 'pages/home_page.dart';
+import 'utils/hex_color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +18,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color primary = HexColor.fromHex("#B0FE76");
+    Color secondary = HexColor.fromHex("#81E979");
+    Color background = HexColor.fromHex("#363635");
+
     return GetMaterialApp(
       title: 'N2 Mobile - Games',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme(
+          primary: primary,
+          primaryContainer: primary.withOpacity(0.85),
+          secondary: secondary,
+          secondaryContainer: secondary.withOpacity(0.7),
+          surface: background,
+          error: Colors.redAccent,
+          onPrimary: Colors.black,
+          onSecondary: Colors.white,
+          onSurface: Colors.white,
+          onError: Colors.white,
+          brightness: Brightness.dark,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: background,
+          titleTextStyle: TextStyle(color: primary, fontSize: 20),
+          iconTheme: IconThemeData(color: primary),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(150, 50),
+          ),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: secondary,
+          foregroundColor: Colors.black,
+        ),
+        scaffoldBackgroundColor: background,
         useMaterial3: true,
       ),
       home: HomePage(),
